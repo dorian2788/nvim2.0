3e6006c37b7e50bca95cba249d702309f58dfcb0
dorian2788 <doris.tran09@protonmail.com>
2024-11-07 21:22:42 +1100

    Added the final plugins

--- lua/user/core/keymaps.lua
+++ lua/user/core/keymaps.lua
@@ -4,52 +4,55 @@ local keymap = vim.keymap.set
 -- Normal --
 --
 -- Y behave like captial letters
-keymap("n", "Y", "y$", {desc = ""})
+keymap("n", "Y", "y$", { desc = "" })
+
+keymap("n", "<cmd>w<CR>", "<cmd>wa<CR>", { desc = "Save all" })
 
 -- Centered
-keymap("n", "n", "nzzzv", {desc = "Brings search results to midscreen"})
-keymap("n", "N", "Nzzzv", {desc = "Brings search results to midscreen"})
-keymap("n", "J", "mxJ`z", {desc = "Brings you to the thing you marked with z"})
+keymap("n", "n", "nzzzv", { desc = "Brings search results to midscreen" })
+keymap("n", "N", "Nzzzv", { desc = "Brings search results to midscreen" })
+keymap("n", "J", "mxJ`z", { desc = "Brings you to the thing you marked with z" })
 
 -- keymap("n", "<leader>e", ":Explore<CR>", {desc = ""})
 
 -- Better window navigation
-keymap("n", "<leader>v", ":vsplit<CR>", {desc = "Vertical split"})
-keymap("n", "<leader>h", ":wincmd h<CR>", {desc = "Horizontal split"})
-keymap("n", "<leader>j", ":wincmd j<CR>", {desc = "Move down"})
-keymap("n", "<leader>k", ":wincmd k<CR>", {desc = "Move up"})
-keymap("n", "<leader>l", ":wincmd l<CR>", {desc = "Move right"})
-keymap("n", "<leader>se", "<C-w>=", {desc = "Move left"})
+keymap("n", "<leader>h", ":wincmd h<CR>", { desc = "Horizontal split" })
+keymap("n", "<leader>j", ":wincmd j<CR>", { desc = "Move down" })
+keymap("n", "<leader>k", ":wincmd k<CR>", { desc = "Move up" })
+keymap("n", "<leader>l", ":wincmd l<CR>", { desc = "Move right" })
+keymap("n", "<leader>sv", ":vsplit<CR>", { desc = "Vertical split" })
+keymap("n", "<leader>se", "<C-w>=", { desc = "Move left" })
+keymap("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
-keymap("n", "<leader>+", ":vertical resize +5<CR>", {desc = "make window bigger"})
-keymap("n", "<leader>-", ":vertical resize -5<CR>", {desc = "make window smaller"})
+keymap("n", "<leader>+", ":vertical resize +5<CR>", { desc = "make window bigger" })
+keymap("n", "<leader>-", ":vertical resize -5<CR>", { desc = "make window smaller" })
 
 -- Clear highlights
-keymap("n", "<leader>nh", ":nohlsearch<CR>", {desc = "remove the search highlights"})
+keymap("n", "<leader>nh", ":nohlsearch<CR>", { desc = "remove the search highlights" })
 
 --Close buffers
-keymap("n", "<leader>b", ":w<bar>%bd<bar>e#<CR>", {desc = "close buffers"})
+keymap("n", "<leader>b", ":w<bar>%bd<bar>e#<CR>", { desc = "close buffers" })
 
 -- cpy clipboard
-keymap("n", "<leader>y", '"+y', {desc = "copy to clipboard"})
+keymap("n", "<leader>y", '"+y', { desc = "copy to clipboard" })
 
 -- cpy all
-keymap("n", "<leader>Y", 'gg"+yG', {desc = "copy all to clipboard"})
+keymap("n", "<leader>Y", 'gg"+yG', { desc = "copy all to clipboard" })
 
 -- Insert --
 
 -- undo break points
-keymap("i", ",", ",<C-g>u", {desc = "add breakpoint for ,"})
-keymap("i", ".", ".<C-g>u", {desc = "add breakpoint for ."})
-keymap("i", "!", "!<C-g>u", {desc = "add breakpoint for !"})
-keymap("i", "?", "?<C-g>u", {desc = "add breakpoint for ?"})
+keymap("i", ",", ",<C-g>u", { desc = "add breakpoint for ," })
+keymap("i", ".", ".<C-g>u", { desc = "add breakpoint for ." })
+keymap("i", "!", "!<C-g>u", { desc = "add breakpoint for !" })
+keymap("i", "?", "?<C-g>u", { desc = "add breakpoint for ?" })
 
 -- window management
-keymap("n", "<leader>to", "<cmd>tabnew<CR>", {desc = "Open new tab"})
-keymap("n", "<leader>tx", "<cmd>tabclose<CR>", {desc = "Close current tab"})
-keymap("n", "<leader>tn", "<cmd>tabn<CR>", {desc = "Go to next tab"})
-keymap("n", "<leader>tp", "<cmd>tabp<CR>", {desc = "Go to previous tab"})
-keymap("n", "<leader>tf", "<cmd>tabnew %<CR>", {desc = "Open current buffer in new tab"})
+keymap("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+keymap("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+keymap("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+keymap("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+keymap("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
 
 -- {
 -- }
@@ -61,12 +64,11 @@ keymap("n", "<leader>tf", "<cmd>tabnew %<CR>", {desc = "Open current buffer in n
 -- Visual --
 
 -- Move blocks of text
-keymap("v", "K", ":m '<-2<CR>gv=gv", {desc = "move blocks of text 2 places up"})
-keymap("v", "J", ":m '>+1<cr>gv=gv", {desc = "move blocks of text 1 places down"})
+keymap("v", "K", ":m '<-2<CR>gv=gv", { desc = "move blocks of text 2 places up" })
+keymap("v", "J", ":m '>+1<cr>gv=gv", { desc = "move blocks of text 1 places down" })
 
 -- go down one line
-keymap("v", "<leader>p", "_dP", {desc = "Delete text into a blackhole register and then paste"})
-
+keymap("v", "<leader>p", "_dP", { desc = "Delete text into a blackhole register and then paste" })
 
 -- -- Telescope --
 -- keymap("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>", {desc = ""})
